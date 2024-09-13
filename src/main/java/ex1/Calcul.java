4342b6b2e336a7e2def1a7dfedcd594627600cb4
Mickaël Seffar <mickael.seffar@gmail.com>
2024-09-13 13:33:36 +0200

    Exercice 1 - Fin exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class Calcul {
+
+    //Méthode permettant d'additionner deux entiers
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    //Méthode permettant de soustraire deux entiers
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+}
